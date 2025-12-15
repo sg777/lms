6f7a1bb024127681833e3be1738b9f3fae234d12
sg777 <8114482+sg777@users.noreply.github.com>
2025-12-15 01:29:31 +0100

    Change default HSM endpoint to localhost instead of remote server
    - Default HSM endpoint changed from http://159.69.23.29:9090 to http://127.0.0.1:9090
    - Prevents confusion where explorer connects to remote HSM server by default
    - Explorer will now fail at startup if local HSM server is not running (due to validation check)
    - Users must explicitly specify remote HSM endpoint if needed

--- cmd/explorer/main.go
+++ cmd/explorer/main.go
@@ -11,7 +11,7 @@ import (
 func main() {
 	port := flag.Int("port", 8081, "Explorer server port")
 	raftEndpointsStr := flag.String("raft-endpoints", "http://159.69.23.29:8080,http://159.69.23.30:8080,http://159.69.23.31:8080", "Comma-separated list of Raft cluster endpoints")
-	hsmEndpoint := flag.String("hsm-endpoint", "http://159.69.23.29:9090", "HSM server endpoint")
+	hsmEndpoint := flag.String("hsm-endpoint", "http://127.0.0.1:9090", "HSM server endpoint (required - explorer cannot function without HSM server)")
 	flag.Parse()
 
 	raftEndpoints := strings.Split(*raftEndpointsStr, ",")
