4529edb710880c0d622604865ce2fe9736db2923
sg777 <8114482+sg777@users.noreply.github.com>
2025-12-15 02:04:23 +0100

    Add HSM server main.go with blockchain commits enabled by default
    - Created cmd/hsm-server/main.go for easy HSM server startup
    - Blockchain commits enabled by default (can be disabled with -blockchain-enabled=false)
    - Default configuration uses sg777z.chips.vrsc@ identity
    - All index commits will go to both Raft and Verus blockchain by default

--- cmd/hsm-server/main.go
+++ cmd/hsm-server/main.go
@@ -4,28 +4,52 @@ import (
 	"flag"
 	"log"
 	"strings"
-	
+
 	"github.com/verifiable-state-chains/lms/hsm_server"
 )
 
 func main() {
 	port := flag.Int("port", 9090, "HSM server port")
 	raftEndpointsStr := flag.String("raft-endpoints", "http://159.69.23.29:8080,http://159.69.23.30:8080,http://159.69.23.31:8080", "Comma-separated list of Raft cluster endpoints")
-	flag.Parse()
 	
+	// Blockchain configuration (enabled by default)
+	blockchainEnabled := flag.Bool("blockchain-enabled", true, "Enable blockchain commits (default: true)")
+	blockchainRPCURL := flag.String("blockchain-rpc-url", "http://127.0.0.1:22778", "Verus RPC URL")
+	blockchainRPCUser := flag.String("blockchain-rpc-user", "user1172159772", "RPC username")
+	blockchainRPCPassword := flag.String("blockchain-rpc-password", "pass03465d081d1dfd2b74a2b5de27063f44f6843c64bcd63a6797915eb0ffa25707da", "RPC password")
+	blockchainIdentity := flag.String("blockchain-identity", "sg777z.chips.vrsc@", "Verus identity name")
+	
+	flag.Parse()
+
 	raftEndpoints := strings.Split(*raftEndpointsStr, ",")
 	for i := range raftEndpoints {
 		raftEndpoints[i] = strings.TrimSpace(raftEndpoints[i])
 	}
-	
-	server, err := hsm_server.NewHSMServer(*port, raftEndpoints)
+
+	// Setup blockchain config (enabled by default)
+	var blockchainConfig *hsm_server.BlockchainConfig
+	if *blockchainEnabled {
+		blockchainConfig = &hsm_server.BlockchainConfig{
+			Enabled:      true,
+			RPCURL:       *blockchainRPCURL,
+			RPCUser:      *blockchainRPCUser,
+			RPCPassword:  *blockchainRPCPassword,
+			IdentityName: *blockchainIdentity,
+		}
+		log.Printf("Blockchain commits: ENABLED (identity=%s, rpc=%s)", *blockchainIdentity, *blockchainRPCURL)
+	} else {
+		log.Printf("Blockchain commits: DISABLED (use -blockchain-enabled=true to enable)")
+	}
+
+	server, err := hsm_server.NewHSMServer(*port, raftEndpoints, blockchainConfig)
 	if err != nil {
 		log.Fatalf("Failed to create HSM server: %v", err)
 	}
+
+	log.Printf("Starting HSM server on port %d", *port)
+	log.Printf("Every index commit will go to BOTH Raft and Verus blockchain (if enabled)")
 	
-	log.Printf("Starting HSM Server on port %d", *port)
 	if err := server.Start(); err != nil {
-		log.Fatalf("HSM Server error: %v", err)
+		log.Fatalf("HSM server error: %v", err)
 	}
 }
-
